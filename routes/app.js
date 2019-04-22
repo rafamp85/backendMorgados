dd60e8d0acb20c5a46aefc904048171361490dbd
Rafael Morales Parra <rafaelmp@gmail.com>
2019-04-22 09:18:28 -0500

    Agregando Express a package.json

--- routes/app.js
+++ routes/app.js
@@ -2,28 +2,16 @@
 var express = require('express');
 var app = express();
 
-var Register = require('../models/register');
+
 
 app.get('/', (req, res, next) =>  {
 
-    Register.find( {}, ( err, registers ) => {
-        
-        if ( err ) {
-            return res.status( 500 ).json({
-                ok: false,
-                message: 'Error cargando Registros',
-                errors: err
-            });
-        }
-
-        res.status( 200 ).json({
-            ok: true,
-            registers
-        });
-
+    res.status( 200 ).json({
+        ok: true,
+        message: 'Request successfuly'
     });
 
-});;
+});
 
 
 module.exports = app;
